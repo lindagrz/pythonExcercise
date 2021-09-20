71388676e14fcde9a52a1a3816aa46bba71361f1
Linda <lindagrz@users.noreply.github.com>
2021-09-20 18:00:30 +0300

    assignment 1

---
+++ day1.py
@@ -0,0 +1,16 @@
+# print your name 5 times
+print("Linda "*5)
+
+# print Bananananana using "Ba" once and "na" 5 times
+print("Ba"+"na"*5)
+
+# print the number of seconds in a year calculated from months, days, minutes, seconds
+print(12*30*1440*60) #12 months by 30 days, 1 day ~ 1440 min, 60 seconds
+
+# how much a googol is that is 10 to the power of the 100th
+print(10**100)
+
+# how much will your 1000 Euro investment be worth after 12 years of 6% yearly interest?
+print(1000*1.06**12)
+
+# You can submit a .py file or a link to your code repository on Github.
