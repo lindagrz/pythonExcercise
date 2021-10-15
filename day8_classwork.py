1e952d87de0c78e06cf40548007fb836f52849f0
Linda <lindagrz@users.noreply.github.com>
2021-10-15 16:41:52 +0300

    Day 8 classwork

--- day8_classwork.py
+++ day8_classwork.py
@@ -23,6 +23,7 @@ def get_char_count(text):
 # clean_dict_value ({'a': 5, 'b': 6, 'c': 5}, 5, 10) -> {'a': 10, 'b': 6, 'c': 10}, because 5 was the value to be
 # replaced.
 
+
 def replace_dict_value(d, bad_val, good_val):
     for key, value in d.items():
         if value == bad_val:
