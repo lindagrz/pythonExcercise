1dd29be34b0b7b1b5437b6588becb61ae64d03e3
Linda <lindagrz@users.noreply.github.com>
2021-11-01 18:50:24 +0200

    Day 13 classwork

--- day13_classwork.py
+++ day13_classwork.py
@@ -1,28 +1,41 @@
 import requests
 import json
 import time
-from operator import itemgetter
 
 
 def main():
     url = "https://api.punkapi.com/v2/beers"
     response = requests.get(url)
+    print("Status", response.status_code)
 
     while True:
-        time.sleep(0.3)
-        picked_answers = []
-
-        # this will search all descriptions
-        search = input("\nWhat kind of a beer are you looking for? (input a keyword or 'Q' to exit)\n")
+        search = input("\nWhat kind of a beer are you looking for?\nInput a keyword or 'Q' to exit: ")
+        # an empty line will print all - that's not a bug it's a feature
 
         if search.lower() == 'q':
             break
+
+        picked_answers = {}
+
         for beer_data in response.json():
-            if search in beer_data['description'].lower():
-                print(beer_data['name'], "-", beer_data['description'])
-                picked_answers.append(itemgetter('name', 'description')(beer_data))
-        with open("beer_data.json", mode="a") as write_file:
-            json.dump(picked_answers, write_file, indent=4)
+            if search.lower() in beer_data.get('description').lower():
+                name = beer_data.get("name")
+                tagline = beer_data.get("tagline")
+                description = beer_data.get("description")
+                abv = "ABV", beer_data.get("abv")
+                ibu = "IBU", beer_data.get("ibu")
+                attenuation_level = "Attenuation", beer_data.get("attenuation_level")
+                image_url = beer_data.get("image_url")
+                picked_answers[name] = tagline, description, abv, ibu, attenuation_level, image_url
+
+        if picked_answers == {}:
+            print("No items found!")
+        else:
+            print(f"Number of beers matching criteria: {len(picked_answers)} (details exported to file).")
+            with open(f"beer_search_{search}.json", mode="w") as write_file:
+                json.dump(picked_answers, write_file, indent=4)
+
+        time.sleep(0.3)
 
 
 if __name__ == "__main__":
