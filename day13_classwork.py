b1168d46985a09409cebe55eefd09510d387b721
Linda <lindagrz@users.noreply.github.com>
2021-11-01 17:30:31 +0200

    Day 13 classwork

---
+++ day13_classwork.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import time
+from operator import itemgetter
+
+
+def main():
+    url = "https://api.punkapi.com/v2/beers"
+    response = requests.get(url)
+    # print(response.status_code)
+
+    # for i in range(10):
+    #     time.sleep(0.3)
+    #     beer_data = response.json()
+
+    while True:
+        time.sleep(0.3)
+        picked_answers = []
+
+        # this will search all descriptions
+        search = input("\nWhat kind of a beer are you looking for? (input a keyword or 'Q' to exit)\n")
+
+        if search.lower() == 'q':
+            break
+        for beer_data in response.json():
+            if search in beer_data['description'].lower():
+                print(beer_data['name'], "-", beer_data['description'])
+                picked_answers.append(itemgetter('name', 'description')(beer_data))
+        with open("beer_data.json", mode="w+") as write_file:
+            json.dump(picked_answers, write_file, indent=4)
+
+
+if __name__ == "__main__":
+    main()
