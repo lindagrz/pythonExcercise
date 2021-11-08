f32f07051547440d0fe7695475b40a5fe03aec6e
Linda <lindagrz@users.noreply.github.com>
2021-11-08 10:48:33 +0200

    Day 14 homework

--- day14_classwork.py
+++ day14_classwork.py
@@ -20,36 +20,31 @@ import time
 import json
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
 driver.get('https://www.ss.com/')
-
 time.sleep(5)
-
 cars_element = driver.find_element_by_id("mtd_97")
-
 cars_element.click()
 
 subaru = driver.find_element_by_id("ahc_159")
-
 page_title = subaru.get_attribute('title')
 page_title = ''.join([i for i in page_title if i.isalpha()])
-
 subaru.click()
 
 table_rows = driver.find_elements_by_tag_name("tr")
-print("Found", len(table_rows), "elements")
+# print("Found", len(table_rows), "elements")
 
 ad_rows = [row for row in table_rows if row.get_attribute("id") and row.get_attribute("id").startswith("tr_")]
-print(len(ad_rows))
-
 add_text = [[add.text] for add in ad_rows]
-print(add_text)
+# print(add_text)
 
 headline = driver.find_element_by_id("head_line")
-
 head_cells = headline.find_elements_by_tag_name("td")
 header_text = [add.text for add in head_cells]
-print(header_text)
+# print(header_text)
 
-with open(f"{page_title}.json", mode="w", encoding="UTF-8") as write_file:
-    json.dump([header_text, add_text], write_file, indent=4)
+if len(add_text) >= 1:
+    print(f"Found {len(add_text)} ads")
+    with open(f"{page_title}.json", mode="w", encoding="UTF-8") as write_file:
+        json.dump([header_text] + add_text, write_file, indent=4)
+else:
+    print("Insufficient amount of data found!")
