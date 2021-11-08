a859ef981b2677c33e688bef601700d6b9166660
Linda <lindagrz@users.noreply.github.com>
2021-11-08 17:49:42 +0200

    Day 15 classwork

--- day15_classwork.py
+++ day15_classwork.py
@@ -1,3 +1,5 @@
+# TO DO: the following 5 functions for CRUD(create, read,update,delete) operations that work with sqlite database called chinook.db
+#
 # Database is here: https://prosemind.com/mod/resource/view.php?id=88
 #
 # ER diagram is here: https://prosemind.com/mod/page/view.php?id=89
@@ -15,28 +17,44 @@ def create_connection(dbpath):
 def create_artist(conn, artist_name):
     # do not have to return anything but can use try: inside this function
     cur = conn.cursor()
-    cur.execute('INSERT INTO artist (Name) VALUES (?)', artist_name)
+    cur.execute('INSERT INTO artists (Name) VALUES (?)', (artist_name,))
     conn.commit()
 
 
 def read_artists(conn):  # can add some extra parameters to limit
-    conn.execute('SELECT * FROM artist')
-    artists = conn.fetchall()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM artists limit 10')
+    artists = cur.fetchall()
     return artists  # can return a list of tuples, or you can create a list of artist objects if you want
 
-# def update_artist(id, new_name):
-#
-# def delete_artist(id=None, name=""):
 
-   # provide deletion by id AND/OR name
+def update_artist(conn, id, new_name):
+    cur = conn.cursor()
+    cur.execute('UPDATE artists SET name = (?) WHERE artistid = (?)', (new_name, id, ))
+    conn.commit()
+
+
+def delete_artist(conn, id=None, name=""):
+    cur = conn.cursor()
+
+    if name == "" and not id is None:
+        cur.execute('DELETE FROM artists WHERE artistid = (?)', (id, ))
+    elif not name == "" and id is None:
+        cur.execute('DELETE FROM artists WHERE name = (?)', (name, ))
+    elif not name == "" and not id is None:
+        cur.execute('DELETE FROM artists WHERE artistid = (?) and name = (?)', (id, name,))
+    conn.commit()
 
 
 def main():
-    con = create_connection("chinook.db")
-    # create_artist(con, 'Linda')
+    con = create_connection('chinook.db')
+    create_artist(con, 'Rammstein')
+    print(read_artists(con))
+    update_artist(con, 3, 'Something else')
+    print(read_artists(con))
+    delete_artist(con, name='Something else')
     print(read_artists(con))
 
-    con.commit()
     con.close()
 
 
